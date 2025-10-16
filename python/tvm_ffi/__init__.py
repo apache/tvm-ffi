0a5cf7cf54319285555dbb6b5dafd8f3f2f4129a
Junru Shao <junrushao@apache.org>
2025-10-16 14:19:06 -0700

    chore(build): Introduce `setuptools_scm` for better versioning

--- python/tvm_ffi/__init__.py
+++ python/tvm_ffi/__init__.py
@@ -18,7 +18,6 @@
 
 # order matters here so we need to skip isort here
 # isort: skip_file
-__version__ = "0.1.0"
 
 # HACK: try importing torch first, to avoid a potential
 # symbol conflict when both torch and tvm_ffi are imported.
@@ -60,6 +59,13 @@ from . import dataclasses
 # optional module to speedup dlpack conversion
 from . import _optional_torch_c_dlpack
 
+
+try:
+    from ._version import __version__, __version_tuple__  # type: ignore[import-not-found]
+except ImportError:
+    __version__ = version = "0.0.0.dev0"
+    __version_tuple__ = version_tuple = (0, 0, 0, "dev0", "7d34eb8ab.d20250913")
+
 __all__ = [
     "Array",
     "DLDeviceType",
@@ -74,6 +80,7 @@ __all__ = [
     "Shape",
     "Tensor",
     "__version__",
+    "__version_tuple__",
     "access_path",
     "convert",
     "dataclasses",
