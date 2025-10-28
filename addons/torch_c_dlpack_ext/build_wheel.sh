e21b89417351c7b56e39703db6af85c521dbee80
Yaxing Cai <caiyaxing666@gmail.com>
2025-10-28 03:57:25 +0000

    upd

--- addons/torch_c_dlpack_ext/build_wheel.sh
+++ addons/torch_c_dlpack_ext/build_wheel.sh
@@ -1,13 +1,13 @@
 pip install uv
 
-for torch_minor in {4..9}
+for torch_minor in {8..9}
 do
   uv venv /torch2$torch_minor && source /torch26/bin/activate
   uv pip install torch==2.$torch_minor --index-url https://download.pytorch.org/whl/cu126
   pip install -v .
-#   python python/tvm_ffi/utils/_build_optional_c_dlpack.py --build_dir ./addons/torch_c_dlpack_ext/torch_c_dlpack_ext
-  echo "torch2$torch_minor" >> ./addons/torch_c_dlpack_ext/torch_c_dlpack_ext/libtorch_c_dlpack_addon.so
+  python python/tvm_ffi/utils/_build_optional_c_dlpack.py --build_dir ./addons/torch_c_dlpack_ext/torch_c_dlpack_ext
   mv ./addons/torch_c_dlpack_ext/torch_c_dlpack_ext/libtorch_c_dlpack_addon.so ./addons/torch_c_dlpack_ext/torch_c_dlpack_ext/libtorch_c_dlpack_addon_torch2$torch_minor.so
+  rm /torch2$torch_minor -rf
 done
 
 cd ./addons/torch_c_dlpack_ext
