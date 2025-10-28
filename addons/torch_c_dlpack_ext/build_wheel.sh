aa4f76ba1b5dcfa04c7b74355911b4c7ba4dc3f9
Yaxing Cai <caiyaxing666@gmail.com>
2025-10-28 22:10:50 +0000

    fix

--- addons/torch_c_dlpack_ext/build_wheel.sh
+++ addons/torch_c_dlpack_ext/build_wheel.sh
@@ -22,12 +22,12 @@ set -o pipefail
 
 pip install uv
 
-./addons/torch_c_dlpack_ext/build_so.sh 2.4 "$1" cu124
-./addons/torch_c_dlpack_ext/build_so.sh 2.5 "$1" cu124
-./addons/torch_c_dlpack_ext/build_so.sh 2.6 "$1" cu126
-./addons/torch_c_dlpack_ext/build_so.sh 2.7 "$1" cu126
-./addons/torch_c_dlpack_ext/build_so.sh 2.8 "$1" cu128
-./addons/torch_c_dlpack_ext/build_so.sh 2.9 "$1" cu128
+bash ./addons/torch_c_dlpack_ext/build_so.sh 2.4 "$1" cu124
+bash ./addons/torch_c_dlpack_ext/build_so.sh 2.5 "$1" cu124
+bash ./addons/torch_c_dlpack_ext/build_so.sh 2.6 "$1" cu126
+bash ./addons/torch_c_dlpack_ext/build_so.sh 2.7 "$1" cu126
+bash ./addons/torch_c_dlpack_ext/build_so.sh 2.8 "$1" cu128
+bash ./addons/torch_c_dlpack_ext/build_so.sh 2.9 "$1" cu128
 
 uv venv /base --python "$1" && source /base/bin/activate
 uv pip install build auditwheel
