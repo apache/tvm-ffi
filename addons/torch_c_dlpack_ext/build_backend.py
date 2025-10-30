39201ecda560bc891083dc2a24721722bc6d6b0a
Yaxing Cai <caiyaxing666@gmail.com>
2025-10-30 03:58:34 +0000

    fix

--- addons/torch_c_dlpack_ext/build_backend.py
+++ addons/torch_c_dlpack_ext/build_backend.py
@@ -63,7 +63,7 @@ def build_wheel(
     config_settings: orig._ConfigSettings = None,
     metadata_directory: orig.StrPath | None = None,
 ) -> str:
-    """Custom build wheel."""
+    """Build wheel."""
     if not _is_lib_prebuilt():
         # build wheel from sdist package, compile the torch c dlpack ext library locally.
         import torch  # noqa: PLC0415
