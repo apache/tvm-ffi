52127decb85d15d94245787df7afc75a574b6aae
Yaxing Cai <caiyaxing666@gmail.com>
2025-11-09 04:16:14 +0000

    try

--- addons/torch_c_dlpack_ext/build_aot_wheels.bat
+++ addons/torch_c_dlpack_ext/build_aot_wheels.bat
@@ -8,62 +8,60 @@ echo python_version=%python_version%
 
 set tvm_ffi=%cd%
 set torch_c_dlpack_ext=%tvm_ffi%\addons\torch_c_dlpack_ext
-set torch_version=
 
 for %%P in (2.4 2.5 2.6 2.7 2.8 2.9) do (
-    set torch_version=%%P
-    call :build_libs
+    call :build_libs %%P
 )
 
 :get_torch_url
-    if !torch_version!==2.4 (set %~2=https://download.pytorch.org/whl/cu124 & exit /b 0)
-    if !torch_version!==2.5 (set %~2=https://download.pytorch.org/whl/cu124 & exit /b 0)
-    if !torch_version!==2.6 (set %~2=https://download.pytorch.org/whl/cu126 & exit /b 0)
-    if !torch_version!==2.7 (set %~2=https://download.pytorch.org/whl/cu128 & exit /b 0)
-    if !torch_version!==2.8 (set %~2=https://download.pytorch.org/whl/cu129 & exit /b 0)
-    if !torch_version!==2.9 (set %~2=https://download.pytorch.org/whl/cu129 & exit /b 0)
-    echo Unknown or unsupported torch version: !torch_version! >&2
+    set torch_version=%1
+    if %torch_version%==2.4 (set %~2=https://download.pytorch.org/whl/cu124 & exit /b 0)
+    if %torch_version%==2.5 (set %~2=https://download.pytorch.org/whl/cu124 & exit /b 0)
+    if %torch_version%==2.6 (set %~2=https://download.pytorch.org/whl/cu126 & exit /b 0)
+    if %torch_version%==2.7 (set %~2=https://download.pytorch.org/whl/cu128 & exit /b 0)
+    if %torch_version%==2.8 (set %~2=https://download.pytorch.org/whl/cu129 & exit /b 0)
+    if %torch_version%==2.9 (set %~2=https://download.pytorch.org/whl/cu129 & exit /b 0)
+    echo Unknown or unsupported torch version: %torch_version% >&2
     set %~2=
     exit /b 1
 
 :check_availability
     set torch_version=%1
-    if !torch_version!==2.4 (
+    if %torch_version%==2.4 (
         if %python_version%==cp313 exit /b 1
         if %python_version%==cp314 exit /b 1
         exit /b 0
     )
-    if !torch_version!==2.5 (
+    if %torch_version%==2.5 (
         if %python_version%==cp314 exit /b 1
         exit /b 0
     )
-    if !torch_version!==2.6 (
+    if %torch_version%==2.6 (
         if %python_version%==cp314 exit /b 1
         exit /b 0
     )
-    if !torch_version!==2.7 (
+    if %torch_version%==2.7 (
         if %python_version%==cp314 exit /b 1
         exit /b 0
     )
-    if !torch_version!==2.8 (
+    if %torch_version%==2.8 (
         if %python_version%==cp314 exit /b 1
         exit /b 0
     )
-    if !torch_version!==2.9 (
+    if %torch_version%==2.9 (
         if %python_version%==cp39 exit /b 1
         exit /b 0
     )
-    echo Unknown or unsupported torch version: !torch_version! >&2
+    echo Unknown or unsupported torch version: %torch_version% >&2
     exit /b 1
 
 :build_libs
-    setlocal
-    call :check_availability !torch_version!
+    set torch_version=%1
+    call :check_availability %torch_version%
     if %errorlevel%==0 (
-        call :get_torch_url !torch_version! torch_url
-        echo %arch% %python_version% !torch_version! %torch_url%
+        call :get_torch_url %torch_version% torch_url
+        echo %arch% %python_version% %torch_version% %torch_url%
     ) else (
-        echo Skipping build for torch !torch_version! on %arch% with python %python_version% as it is not available.
+        echo Skipping build for torch %torch_version% on %arch% with python %python_version% as it is not available.
     )
-    endlocal
     exit /b 0
