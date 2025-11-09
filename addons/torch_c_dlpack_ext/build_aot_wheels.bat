4f61dab9e7f8029ca3c1d5d96428f9f830e6d3e9
Yaxing Cai <caiyaxing666@gmail.com>
2025-11-09 04:14:06 +0000

    try

--- addons/torch_c_dlpack_ext/build_aot_wheels.bat
+++ addons/torch_c_dlpack_ext/build_aot_wheels.bat
@@ -8,6 +8,7 @@ echo python_version=%python_version%
 
 set tvm_ffi=%cd%
 set torch_c_dlpack_ext=%tvm_ffi%\addons\torch_c_dlpack_ext
+set torch_version=
 
 for %%P in (2.4 2.5 2.6 2.7 2.8 2.9) do (
     set torch_version=%%P
