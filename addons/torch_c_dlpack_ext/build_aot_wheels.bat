3eba302206d245cf169b3f9050196f767a9e97ed
Yaxing Cai <caiyaxing666@gmail.com>
2025-11-09 04:08:23 +0000

    debug

--- addons/torch_c_dlpack_ext/build_aot_wheels.bat
+++ addons/torch_c_dlpack_ext/build_aot_wheels.bat
@@ -9,66 +9,60 @@ echo python_version=%python_version%
 set tvm_ffi=%cd%
 set torch_c_dlpack_ext=%tvm_ffi%\addons\torch_c_dlpack_ext
 
-for %%P in ("2.4" "2.5" "2.6" "2.7" "2.8" "2.9") do (
-    call :build_libs %%~P
+for %%P in (2.4 2.5 2.6 2.7 2.8 2.9) do (
+    set torch_version=%%P
+    call :build_libs
 )
 
 :get_torch_url
-    setlocal
-    set version=%~1
-    if "%version%"=="2.4" (
-        set url=https://download.pytorch.org/whl/cu124
-    ) else if "%version%"=="2.5" (
-        set url=https://download.pytorch.org/whl/cu124
-    ) else if "%version%"=="2.6" (
-        set url=https://download.pytorch.org/whl/cu126
-    ) else if "%version%"=="2.7" (
-        set url=https://download.pytorch.org/whl/cu128
-    ) else if "%version%"=="2.8" (
-        set url=https://download.pytorch.org/whl/cu129
-    ) else if "%version%"=="2.9" (
-        set url=https://download.pytorch.org/whl/cu129
-    ) else (
-        echo Unknown or unsupported torch version: %version% >&2
-        endlocal & set %~2=%url%
-        exit /b 1
-    )
-    endlocal & set %~2=%url%
-    exit /b 0
+    if !torch_version!==2.4 (set %~2=https://download.pytorch.org/whl/cu124 & exit /b 0)
+    if !torch_version!==2.5 (set %~2=https://download.pytorch.org/whl/cu124 & exit /b 0)
+    if !torch_version!==2.6 (set %~2=https://download.pytorch.org/whl/cu126 & exit /b 0)
+    if !torch_version!==2.7 (set %~2=https://download.pytorch.org/whl/cu128 & exit /b 0)
+    if !torch_version!==2.8 (set %~2=https://download.pytorch.org/whl/cu129 & exit /b 0)
+    if !torch_version!==2.9 (set %~2=https://download.pytorch.org/whl/cu129 & exit /b 0)
+    echo Unknown or unsupported torch version: !torch_version! >&2
+    set %~2=
+    exit /b 1
 
 :check_availability
-    setlocal
     set torch_version=%1
-    set return=0
-    if "%torch_version%"=="2.4" (
-        if "%python_version%"=="cp313" set return=1
-        if "%python_version%"=="cp314" set return=1
-    ) else if "%torch_version%"=="2.5" (
-        if "%python_version%"=="cp314" set return=1
-    ) else if "%torch_version%"=="2.6" (
-        if "%python_version%"=="cp314" set return=1
-    ) else if "%torch_version%"=="2.7" (
-        if "%python_version%"=="cp314" set return=1
-    ) else if "%torch_version%"=="2.8" (
-        if "%python_version%"=="cp314" set return=1
-    ) else if "%torch_version%"=="2.9" (
-        if "%python_version%"=="cp39" set return=1
-    ) else (
-        echo Unknown or unsupported torch version: %torch_version% >&2
-        set return=1
+    if !torch_version!==2.4 (
+        if %python_version%==cp313 exit /b 1
+        if %python_version%==cp314 exit /b 1
+        exit /b 0
     )
-    endlocal
-    exit /b %return%
+    if !torch_version!==2.5 (
+        if %python_version%==cp314 exit /b 1
+        exit /b 0
+    )
+    if !torch_version!==2.6 (
+        if %python_version%==cp314 exit /b 1
+        exit /b 0
+    )
+    if !torch_version!==2.7 (
+        if %python_version%==cp314 exit /b 1
+        exit /b 0
+    )
+    if !torch_version!==2.8 (
+        if %python_version%==cp314 exit /b 1
+        exit /b 0
+    )
+    if !torch_version!==2.9 (
+        if %python_version%==cp39 exit /b 1
+        exit /b 0
+    )
+    echo Unknown or unsupported torch version: !torch_version! >&2
+    exit /b 1
 
 :build_libs
     setlocal
-    set torch_version=%1
-    call :check_availability %torch_version%
+    call :check_availability !torch_version!
     if %errorlevel%==0 (
-        call :get_torch_url %torch_version% torch_url
-        echo %arch% %python_version% %torch_version% %torch_url%
+        call :get_torch_url !torch_version! torch_url
+        echo %arch% %python_version% !torch_version! %torch_url%
     ) else (
-        echo Skipping build for torch %torch_version% on %arch% with python %python_version% as it is not available.
+        echo Skipping build for torch !torch_version! on %arch% with python %python_version% as it is not available.
     )
     endlocal
     exit /b 0
