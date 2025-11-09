ed236969df22e0fa6b44780121ead5626c853405
Yaxing Cai <caiyaxing666@gmail.com>
2025-11-09 06:27:25 +0000

    fix

--- addons/torch_c_dlpack_ext/build_aot_wheels.bat
+++ addons/torch_c_dlpack_ext/build_aot_wheels.bat
@@ -71,19 +71,19 @@ exit /b 0
     call :check_availability
     if %errorlevel%==0 (
         call :get_torch_url torch_url
-        echo fff %arch% %python_version% %torch_version% %torch_url%
-        @REM mkdir %tvm_ffi%\.venv
-        @REM uv venv %tvm_ffi%\.venv\torch%torch_version% --python %python_version%
-        @REM %tvm_ffi%\.venv\torch%torch_version%\Scripts\activate
-        @REM uv pip install setuptools ninja
-        @REM uv pip install torch==%torch_version% --index-url %torch_url%
-        @REM uv pip install -v .
-        @REM mkdir %tvm_ffi%\lib
-        @REM python -m tvm_ffi.utils._build_optional_torch_c_dlpack --output-dir %tvm_ffi%\lib
-        @REM python -m tvm_ffi.utils._build_optional_torch_c_dlpack --output-dir %tvm_ffi%\lib --build-with-cuda
-        @REM dir %tvm_ffi%\lib
-        @REM deactivate
-        @REM rmdir -s -q %tvm_ffi%\.venv\torch%torch_version%
+        echo %arch% %python_version% %torch_version% %torch_url%
+        mkdir %tvm_ffi%\.venv
+        uv venv %tvm_ffi%\.venv\torch%torch_version% --python %python_version%
+        %tvm_ffi%\.venv\torch%torch_version%\Scripts\activate
+        uv pip install setuptools ninja
+        uv pip install torch==%torch_version% --index-url %torch_url%
+        uv pip install -v .
+        mkdir %tvm_ffi%\lib
+        python -m tvm_ffi.utils._build_optional_torch_c_dlpack --output-dir %tvm_ffi%\lib
+        python -m tvm_ffi.utils._build_optional_torch_c_dlpack --output-dir %tvm_ffi%\lib --build-with-cuda
+        dir %tvm_ffi%\lib
+        deactivate
+        rmdir -s -q %tvm_ffi%\.venv\torch%torch_version%
     ) else (
         echo Skipping build for torch %torch_version% on %arch% with python %python_version% as it is not available.
     )
