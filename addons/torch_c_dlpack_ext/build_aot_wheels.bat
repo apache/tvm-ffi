dcb7740db981102c7ca1370deb48c4b51f3019cb
Yaxing Cai <caiyaxing666@gmail.com>
2025-11-09 03:43:59 +0000

    upd

--- addons/torch_c_dlpack_ext/build_aot_wheels.bat
+++ addons/torch_c_dlpack_ext/build_aot_wheels.bat
@@ -7,10 +7,7 @@ echo arch=%arch%
 echo python_version=%python_version%
 
 set tvm_ffi=%cd%
-set torch_c_dlpack_ext=%tvm_ffi%/addons/torch_c_dlpack_ext
-
-echo tvm_ffi=%tvm_ffi%
-echo torch_c_dlpack_ext=%torch_c_dlpack_ext%
+set torch_c_dlpack_ext=%tvm_ffi%\addons\torch_c_dlpack_ext
 
 for %%P in ("2.4" "2.5" "2.6" "2.7" "2.8" "2.9") do (
     call :build_libs %%~P
@@ -20,44 +17,44 @@ for %%P in ("2.4" "2.5" "2.6" "2.7" "2.8" "2.9") do (
     setlocal
     set version=%~1
     if "%version%"=="2.4" (
-        set url="https://download.pytorch.org/whl/cu124"
+        set url=https://download.pytorch.org/whl/cu124
     ) else if "%version%"=="2.5" (
-        set url="https://download.pytorch.org/whl/cu124"
+        set url=https://download.pytorch.org/whl/cu124
     ) else if "%version%"=="2.6" (
-        set url="https://download.pytorch.org/whl/cu126"
+        set url=https://download.pytorch.org/whl/cu126
     ) else if "%version%"=="2.7" (
-        set url="https://download.pytorch.org/whl/cu128"
+        set url=https://download.pytorch.org/whl/cu128
     ) else if "%version%"=="2.8" (
-        set url="https://download.pytorch.org/whl/cu129"
+        set url=https://download.pytorch.org/whl/cu129
     ) else if "%version%"=="2.9" (
-        set url="https://download.pytorch.org/whl/cu129"
+        set url=https://download.pytorch.org/whl/cu129
     ) else (
-        echo  "Unknown or unsupported torch version: %version%" >&2
+        echo Unknown or unsupported torch version: %version% >&2
+        endlocal & set %~2=%url%
         exit /b 1
     )
-    endlocal & set "%~2=%url%"
+    endlocal & set %~2=%url%
     exit /b 0
 
 :check_availability
     setlocal
     set torch_version=%1
-    echo %torch_version%
     set return=0
     if "%torch_version%"=="2.4" (
         if "%python_version%"=="cp313" set return=1
         if "%python_version%"=="cp314" set return=1
-    ) else if "%version%"=="2.5" (
+    ) else if "%torch_version%"=="2.5" (
         if "%python_version%"=="cp314" set return=1
-    ) else if "%version%"=="2.6" (
+    ) else if "%torch_version%"=="2.6" (
         if "%python_version%"=="cp314" set return=1
-    ) else if "%version%"=="2.7" (
+    ) else if "%torch_version%"=="2.7" (
         if "%python_version%"=="cp314" set return=1
-    ) else if "%version%"=="2.8" (
+    ) else if "%torch_version%"=="2.8" (
         if "%python_version%"=="cp314" set return=1
-    ) else if "%version%"=="2.9" (
+    ) else if "%torch_version%"=="2.9" (
         if "%python_version%"=="cp39" set return=1
     ) else (
-        echo Unknown or unsupported torch version: %version% >&2
+        echo Unknown or unsupported torch version: %torch_version% >&2
         set return=1
     )
     endlocal
@@ -66,7 +63,6 @@ for %%P in ("2.4" "2.5" "2.6" "2.7" "2.8" "2.9") do (
 :build_libs
     setlocal
     set torch_version=%1
-    echo %torch_version%
     call :check_availability %torch_version%
     if %errorlevel%==0 (
         call :get_torch_url %torch_version% torch_url
