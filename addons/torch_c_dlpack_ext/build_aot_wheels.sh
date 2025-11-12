2d39718fe2bb0009cb87f7fce9eb6c00e52ac981
Yaxing Cai <caiyaxing666@gmail.com>
2025-11-12 06:21:13 +0000

    debug

--- addons/torch_c_dlpack_ext/build_aot_wheels.sh
+++ addons/torch_c_dlpack_ext/build_aot_wheels.sh
@@ -127,9 +127,9 @@ python -m wheel tags dist/*.whl --python-tag="$python_version" --abi-tag="$pytho
 ls dist
 if [[ "$os" == "Linux" ]]; then
     uv pip install auditwheel
-    auditwheel repair --exclude libtorch.so --exclude libtorch_cpu.so --exclude libc10.so --exclude libtorch_python.so --exclude libtorch_cuda.so dist/*.whl -w wheelhouse
+    auditwheel repair --exclude libtorch.so --exclude libtorch_cpu.so --exclude libc10.so --exclude libtorch_python.so --exclude libtorch_cuda.so --exclude libc10_cuda.so dist/*.whl -w wheelhouse
 else
     uv pip install delocate
-    delocate-wheel -v --exclude libtorch.dylib,libtorch_cpu.dylib,libc10.dylib,libtorch_python.dylib dist/*.whl -w wheelhouse
+    delocate-wheel -v --ignore-missing-dependencies --exclude libtorch.dylib,libtorch_cpu.dylib,libc10.dylib,libtorch_python.dylib dist/*.whl -w wheelhouse
 fi
 ls wheelhouse
