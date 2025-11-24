6ae4991f7dbabf90540e98ac43f6748cc5dbe1c3
Yaxing Cai <caiyaxing666@gmail.com>
2025-11-24 04:54:59 +0000

    upd

--- addons/tvm-ffi-orcjit/python/tvm_ffi_orcjit/_ffi_api.py
+++ addons/tvm-ffi-orcjit/python/tvm_ffi_orcjit/_ffi_api.py
@@ -13,7 +13,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""FFI APIs for orcjit"""
+"""FFI APIs for orcjit."""
 
 import tvm_ffi
 
