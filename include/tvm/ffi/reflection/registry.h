d3043444b39ae1651a0f736e27e696f2dd956c09
DarkSharpness <2040703891@qq.com>
2025-11-27 03:41:57 +0800

    fix: docs fix

--- include/tvm/ffi/reflection/registry.h
+++ include/tvm/ffi/reflection/registry.h
@@ -586,6 +586,9 @@ class ObjectDef : public ReflectionDefBase {
   }
 
  private:
+  template <typename T>
+  friend class OverloadObjectDef;
+
   template <typename... ExtraArgs>
   void RegisterExtraInfo(ExtraArgs&&... extra_args) {
     TVMFFITypeMetadata info;
@@ -656,8 +659,6 @@ class ObjectDef : public ReflectionDefBase {
     TVM_FFI_CHECK_SAFE_CALL(TVMFFITypeRegisterMethod(type_index_, &info));
   }
 
- protected:
-  // These fields may be used in subclasses
   int32_t type_index_;
   const char* type_key_;
   static constexpr const char* kInitMethodName = "__ffi_init__";
